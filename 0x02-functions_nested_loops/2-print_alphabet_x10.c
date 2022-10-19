fd4fc4e6998e01c399d07d3b754fc8658d086214
ybitrus <yusufbitrus@gmail.com>
2022-10-19 13:59:07 -0700

    tasks 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,13 +2,11 @@
 
 /**
  *print_alphabet_x10 -> prints the lowercase alphabets
- *
- *Return: Always 0
  */
 
 void print_alphabet_x10(void)
 {
-	char letters;
+	int letters;
 	int t;
 
 	for (t = 0; t < 10; t++)
@@ -16,7 +14,6 @@ void print_alphabet_x10(void)
 	for (letters = 'a'; letters <= 'z'; t++)
 		{
 		_putchar(letters);
-		return (0);
 		}
 	  _putchar('\n');
 	}
