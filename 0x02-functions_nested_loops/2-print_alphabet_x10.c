56240e5f5084cb09531696dc1c2c807db034ea6d
ybitrus <yusufbitrus@gmail.com>
2022-10-19 12:48:05 -0700

    hot fixed tasks 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- *print_alphabet -> prints the lowercase aplphabets
- *
- *Return: 0
+ *print_alphabet_x10 -> prints the lowercase alphabets
  */
 
 void print_alphabet_x10(void)
