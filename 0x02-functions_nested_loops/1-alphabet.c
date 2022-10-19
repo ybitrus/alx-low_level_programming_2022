86ca4837a732cdbe7897ce92365d26bad31e59e2
ybitrus <yusufbitrus@gmail.com>
2022-10-19 05:00:56 -0700

    task 1 on functions and nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_alphabet -> prints the lowercase aplphabets
+ *
+ *Return: 0
+ */
+
+void print_alphabet(void)
+{
+	int my_letters;
+
+	for (my_letters = 'a'; my_letters <= 'z' my_letters++)
+	{
+	_putchar('my_letters');
+	}
+	_putchar('\n');
+
+}
