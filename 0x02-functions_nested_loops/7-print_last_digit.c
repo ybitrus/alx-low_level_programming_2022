e823bab8f3829c8bb79dd8fdff1d3b9b7a426ff7
ybitrus <yusufbitrus@gmail.com>
2022-10-20 10:20:44 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,13 +8,15 @@
 int print_last_digit(int n)
 
 {
-	int ld = n % 10;
+	int ld;
+
+	ld = (n % 10)
 
 	if (ld < 0)
-		ld *= -1;
-
+	{
+	ld = (-1 * ld);
+	}
 	_putchar(ld + '0');
-
-	return (0);
+	return (ld);
 
 }
