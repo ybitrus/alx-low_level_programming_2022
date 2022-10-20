5f01468090301eb79e9899753b064dbea9a762a5
ybitrus <yusufbitrus@gmail.com>
2022-10-20 08:35:05 -0700

    tasks 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign -print the sign of a number
+ *@n: n is the number of sign to be printed
+ * Return: returns 0 if n is zero, -1 if n is less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > o)
+	{
+	_putchar('+');
+	return (1);
+	}
+	else if (n == 0)
+	{
+	_putchar('0');
+	return	(0);
+	}
+	else
+	{
+		_putchar('-');
+			return (-1);
+	}
+}
