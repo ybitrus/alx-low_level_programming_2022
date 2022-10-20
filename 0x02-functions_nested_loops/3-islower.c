b482bd246c0af72b52db85f8e5f4a36f3e877ed3
ybitrus <yusufbitrus@gmail.com>
2022-10-19 19:39:24 -0700

    tasks 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+include "main.h"
+/**
+ *
+ * a function that checks for alphabetic character
+ *
+ * Returns 1 if c is a letter, lowercase or uppercase
+ */
+int int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+	else
+		return (0);
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,4 +2,3 @@ int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _isalpha(int c);
-int _isalpha(int c);
