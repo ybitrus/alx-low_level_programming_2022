b446116061da668ee2eebe4dcdd77309245cb82d
ybitrus <yusufbitrus@gmail.com>
2022-10-20 07:51:12 -0700

    tasks 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,16 @@
 include "main.h"
 
 /**
+ * _isalpha - function that checks for alphabets
+ * @c: alphabet to be checks
  *
- * main: check _isalpha function that checks for alphabetic character
- *
- * Returns 1 if c is a letter, lowercase or uppercase
+ * Return: return 1 if is c else 0
  */
 
 int _isalpha(int c)
 {
 
-	if (c >= 'a' && c <= 'z')
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
