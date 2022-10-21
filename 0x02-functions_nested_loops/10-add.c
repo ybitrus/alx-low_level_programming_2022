0a66a3d3a66c1d0394cc1bc11e3bbe07b1775cc4
ybitrus <yusufbitrus@gmail.com>
2022-10-20 21:33:43 -0700

    tasks10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,9 @@
 #include "main.h"
 /**
  * add - a function that adds two integers and returns the result.
+ * @x:first to be added
+ * @y:sccond to be added
+ * Return: x + y
  */
 int add(int x, int y)
 {
