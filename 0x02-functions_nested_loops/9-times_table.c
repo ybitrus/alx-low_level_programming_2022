f82e2b3ff3ffa89534af9e6bdb52a9d0de0ac3ac
ybitrus <yusufbitrus@gmail.com>
2022-10-25 04:37:15 -0700

    tasks 9 corrections

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,29 +8,29 @@ void times_table(void)
 
 	for (n = 0; n <= 9; n++)
 	{
-		_putchar('0');
-		_putchar(',');
-		_putchar('');
+		_putchar("0");
+		_putchar(",");
+		_putchar("");
 		for (m = 1; m <= 9; m++)
-		:wq{ 
+		{ 
 			p = (n * m);
 			if ((p / 10) > 0)
 			{
-				_putchar((p / 10) + '0');
+				_putchar((p / 10) + "0");
 			}
 			else
 			{
-				_putchar('');
+				_putchar(" ");
 			}
-			_putchar((p % 10) + '0');
+			_putchar((p % 10) + "0");
 			
 			if (m < 9)
 			{
-				_putchar('');
-				_putchar('');
+				_putchar("");
+				_putchar("");
 			}
 		}
 
-		_putchar('\n');
+		_putchar("\n");
 	}
 }
