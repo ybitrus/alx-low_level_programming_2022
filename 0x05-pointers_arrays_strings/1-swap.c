e8604658f4bbb3f83beda4cac5d82f935ae288d1
ybitrus <yusufbitrus@gmail.com>
2022-10-25 06:16:48 -0700

    tasks 1 swapping

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * swap_int - function that swap intergers
+ *
+ * @a: first swaping integers
+ * @b: second swaping integers
+ */
+
+void swap_int(int *a, int *b)
+{
+	int sw;
+
+	sw = *;
+	*a = *b;
+	*b = sw;
+
+
+}
