0989489e3a95870fbfd342bf895551f445301398
ybitrus <yusufbitrus@gmail.com>
2022-10-25 05:50:28 -0700

    new hot fixed

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,12 +4,7 @@
  * reset_to_98 - a function to update
  * @n: that is pointing n
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
-	int x;
-
-	int *n;
-		x = 98;
-		n = &n;
-
+	*n = 98;
 }
