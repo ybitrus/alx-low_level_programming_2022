561a3e74c24f18d542fec1bf066f43687fe5b0c1
ybitrus <yusufbitrus@gmail.com>
2022-10-27 09:36:52 -0700

    betty fix

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,11 +2,12 @@
 /**
  * main - Etry point
  *
- * Return: Always 0 
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	int num;
+
 	for (num = 0; num < 10; num++)
 		printf("%d", num);
 	printf("\n");
