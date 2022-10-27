151fd005950e0588228d53b1b062faf5dc4d5cbd
ybitrus <yusufbitrus@gmail.com>
2022-10-27 10:56:57 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	int d;
+
+	char c = 'a';
+
+	for (d = 0; d < 10; d++)
+	{
+		
+		putchar(d + '0');
+	}
+
+	while (c <= 'f')
+	{
+		putchar(c);
+	c++;
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
