48b7f1432dd7b135811ea47f3315597d6ab5bcaf
ybitrus <yusufbitrus@gmail.com>
2022-10-30 11:57:55 -0700

    tasks 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,17 +6,18 @@
 
 void print_to_98(int n)
 {
+	int n = 0;
 	if (n >= 98)
 	{
 		while (n > 98)
-			printf("%d, ", n--);
-		printf("%d\n", n);
+			_putchar("%d, ", n--);
+		_putchar("%d\n", n);
 	}
 
 	else
 	{
 		while (n < 98)
-			printf("%d, ", n++);
-		printf("%d\n", n);
+			_putchar("%d, ", n++);
+		_putchar("%d\n", n);
 	}
 }
