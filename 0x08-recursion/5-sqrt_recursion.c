16210a24cd879ea1fad4de032aaa4f311781ff2a
ybitrus <yusufbitrus@gmail.com>
2022-11-03 20:21:24 -0700

    tasks 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - return the natural square root
+ *  @n: num of the square root
+ *
+ *  Return: retuning values
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (actual_sqrt_recursion(n, 0));
+}
+
+/**
+ * actual_sqrt_recursion - recursion to find square root
+ * @n: num to calculated
+ * @i: the itrator
+ *
+ * Return: resulting square root
+ */
+
+
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
+}
