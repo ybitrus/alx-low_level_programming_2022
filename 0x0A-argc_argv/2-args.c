67835c2a2d763e413a26598bafc34d940aed4b7c
ybitrus <yusufbitrus@gmail.com>
2022-11-04 21:25:01 -0700

    tasks 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main -  program that prints all arguments it receives.
+ * @argc: counter
+ * @argv: Arguments
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int c = 0;
+
+	if (argc > 0)
+	{
+		while (c < argc)
+	}
+	printf("%s\n", argv[c]);
+	return (0);
+}
