291d54bfd0ac7178259777bb32cb036d107d1c5a
ybitrus <yusufbitrus@gmail.com>
2022-11-04 22:03:23 -0700

    tasks 3 hot fixed

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,19 +11,18 @@
 
 int main(int argc, char *argv[])
 {
-	int num1, num2, prod;
+	int num1, num2,
 
-	if (argc != 3)
+	if (argc == 3)
 	{
-		printf("Error\n");
-		return (1);
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		prod = num1 * num2;
+		printf("%d\n", num1 *num2);
+		return (0);
 	}
+	
+	printf("Error\n");
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	prod = num1 * num2;
-
-	printf("%d\n", prod);
-
-	return (0);
+	return (1);
 }
