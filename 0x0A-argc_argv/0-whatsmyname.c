e9b02ec110757ccbb4707d333fc7f46d997a7c7b
ybitrus <yusufbitrus@gmail.com>
2022-11-04 20:29:04 -0700

    tasks 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - a program that prints its name
+ * @argc: count arguments
+ * @argv: arguments
+ *
+ * Return: Always return 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+
+}
+
